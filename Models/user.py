868f16ee0113010b5c96a050168f01987626a1f7
Ronak <ronak223@gmail.com>
2013-09-23 18:21:34 -0400

    fifth attempt at fixing database connection bug

--- Models/user.py
+++ Models/user.py
@@ -4,17 +4,25 @@ Created on Sep 20, 2013
 @author: Ronak
 
 '''
-
+from urlparse import urlparse
 import os
-
-from pymongo import MongoClient
-
+import pymongo
 
 MONGO_URL = os.environ.get('MONGOHQ_URL')
-client = MongoClient(MONGO_URL);
+#MONGO_URL = os.environ.get('MONGOHQ_URL')
+#client = MongoClient(MONGO_URL);
 # Specify the database
-db = client.app18266596
-user_collection = db.users
+#db = client.app18266596
+#user_collection = db.users
+db=None
+
+if MONGO_URL:
+    # Get a connection
+    conn = pymongo.Connection(MONGO_URL)
+    
+    # Get the database
+    db = conn[urlparse(MONGO_URL).path[1:]]
+    user_collection = db.users
 
 class User(object):
     '''
